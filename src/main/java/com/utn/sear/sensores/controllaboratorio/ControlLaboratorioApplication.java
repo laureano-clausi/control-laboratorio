a158a18e48d15adb14bf61606d028c3fe1d24f7f
Laureano Clausi <laureano.clausi@gmail.com>
2019-07-10 18:14:13 -0300

    first commit

---
+++ src/main/java/com/utn/sear/sensores/controllaboratorio/ControlLaboratorioApplication.java
@@ -0,0 +1,13 @@
+package com.utn.sear.sensores.controllaboratorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlLaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlLaboratorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/sear/sensores/controllaboratorio/ControlLaboratorioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.utn.sear.sensores.controllaboratorio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControlLaboratorioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
