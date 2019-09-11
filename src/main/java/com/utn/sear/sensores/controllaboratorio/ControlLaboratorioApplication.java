77655cdf431b940e0bc7ef021a683f08d808489a
Laureano Clausi <laureano.clausi@gmail.com>
2019-09-11 18:02:34 -0300

    habilito el cron para realizar las lecturas

--- src/main/java/com/utn/sear/sensores/controllaboratorio/ControlLaboratorioApplication.java
+++ src/main/java/com/utn/sear/sensores/controllaboratorio/ControlLaboratorioApplication.java
@@ -2,8 +2,10 @@ package com.utn.sear.sensores.controllaboratorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ControlLaboratorioApplication {
 
 	public static void main(String[] args) {
